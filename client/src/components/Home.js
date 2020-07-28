9fc2c5d903da3297f035843e7cdf6f4fcb4e53a4
Arciiix <artur.nowak100@gmail.com>
2020-07-28 15:03:57 +0200

    Added home page and navigation

---
+++ client/src/components/Home.js
@@ -0,0 +1,141 @@
+import React from "react";
+import ApexCharts from "react-apexcharts";
+import Switch from "react-switch";
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import {
+  AiOutlineHome,
+  AiOutlineBarChart,
+  AiOutlineSetting,
+} from "react-icons/ai";
+import "../styles/Home.css";
+
+class Home extends React.Component {
+  container;
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOn: false,
+      currentTemperature: 25, //DEV
+      currentPage: "Home",
+    };
+  }
+
+  navigationChange(event, newValue) {
+    this.setState({ currentPage: newValue });
+  }
+
+  toogleLight(e) {
+    //DEV
+    this.setState({ isOn: e });
+  }
+  render() {
+    //switch (this.state.currentPage) {
+    // case "Home":
+    return (
+      <div className="container" ref={(el) => (this.container = el)}>
+        <ApexCharts
+          className="currChart"
+          options={{
+            chart: {
+              id: "currentTemp",
+            },
+            plotOptions: {
+              radialBar: {
+                startAngle: -135,
+                endAngle: 135,
+                hollow: {
+                  size: "70%",
+                },
+
+                dataLabels: {
+                  name: {
+                    show: false,
+                  },
+                  value: {
+                    formatter: function (val) {
+                      return parseFloat(val) + "°C";
+                    },
+                    color: "#fff",
+                    fontSize: "1em",
+                  },
+                },
+              },
+            },
+            fill: {
+              type: "gradient",
+              gradient: {
+                shade: "dark",
+                type: "horizontal",
+                shadeIntensity: 0.5,
+                gradientToColors: ["#18cc96"],
+                inverseColors: true,
+                opacityFrom: 1,
+                opacityTo: 1,
+                stops: [0, 100],
+              },
+            },
+            stroke: {
+              lineCap: "round",
+            },
+            labels: ["Percent"],
+            grid: {
+              padding: {
+                bottom: 15,
+                top: 15,
+              },
+            },
+          }}
+          series={[this.state.currentTemperature]}
+          type="radialBar"
+          width={"100%"}
+        />
+        <div className="switch">
+          <Switch
+            checked={this.state.isOn}
+            onChange={this.toogleLight.bind(this)}
+            offColor="#ad1d25"
+            onColor="#86d3ff"
+            onHandleColor="#2693e6"
+            handleDiameter={70}
+            uncheckedIcon={false}
+            checkedIcon={false}
+            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+            height={50}
+            width={100}
+          />
+        </div>
+        <div className="navbar">
+          <BottomNavigation
+            style={{ backgroundColor: "#111413" }}
+            value={this.state.currentPage}
+            onChange={this.navigationChange.bind(this)}
+          >
+            <BottomNavigationAction
+              style={{ color: "white" }}
+              label="Główna"
+              value="Home"
+              icon={<AiOutlineHome size={40} color={"white"} />}
+            />
+            <BottomNavigationAction
+              style={{ color: "white" }}
+              label="Wykres"
+              value="Chart"
+              icon={<AiOutlineBarChart size={40} color={"white"} />}
+            />
+            <BottomNavigationAction
+              style={{ color: "white" }}
+              label="Ustawienia"
+              value="Settings"
+              icon={<AiOutlineSetting size={40} color={"white"} />}
+            />
+          </BottomNavigation>
+        </div>
+      </div>
+    );
+    //  break;
+    //}
+  }
+}
+
+export default Home;
---
+++ client/src/components/Settings.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <h1 style={{ textAlign: "center", fontSize: 100, color: "#eb3449" }}>
+        DEV
+      </h1>
+    );
+  }
+}
+
+export default Settings;
