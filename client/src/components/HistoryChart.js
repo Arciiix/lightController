cbd00829797e9070132ec043d13ba35fc795406b
Arciiix <artur.nowak100@gmail.com>
2020-07-29 11:40:19 +0200

    Created history chart site UI

---
+++ client/src/components/HistoryChart.js
@@ -0,0 +1,52 @@
+import React from "react";
+import ApexCharts from "react-apexcharts";
+import "../styles/Chart.css";
+
+class HistoryChart extends React.Component {
+  render() {
+    return (
+      <ApexCharts
+        options={{
+          chart: {
+            type: "area",
+          },
+          colors: ["#0377fc"],
+          dataLabels: {
+            enabled: false,
+          },
+          stroke: {
+            curve: "smooth",
+          },
+          labels: this.props.data.map((e) => e.time),
+          xaxis: {
+            labels: {
+              show: false,
+            },
+          },
+          yaxis: {
+            labels: {
+              style: {
+                colors: "white",
+              },
+              formatter: function (val) {
+                return parseFloat(val) + "°C";
+              },
+            },
+          },
+        }}
+        series={[
+          {
+            name: "Temperatura",
+            data: this.props.data.map((e) => e.value),
+          },
+        ]}
+        className="historyChart"
+        width={"100%"}
+        type="area"
+        height={"50%"}
+      />
+    );
+  }
+}
+
+export default HistoryChart;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,15 +8,26 @@ import {
   AiOutlineSetting,
 } from "react-icons/ai";
 import "../styles/Home.css";
+import "../styles/Chart.css";
+
+import HistoryChart from "./HistoryChart";
 
 class Home extends React.Component {
-  container;
   constructor(props) {
     super(props);
     this.state = {
       isOn: false,
       currentTemperature: 25, //DEV
+      historyTemperatures: [
+        { time: "12:35", value: 23 },
+        { time: "12:40", value: 25 },
+        { time: "12:45", value: 25 },
+        { time: "12:50", value: 21 },
+        { time: "12:55", value: 23.5 },
+      ], //DEV
       currentPage: "Home",
+      nextChangeText: "Wyłączenie o 22:00", //DEV
+      lastChangeText: "Jeszcze nie przełączono!", //DEV
     };
   }
 
@@ -29,112 +40,169 @@ class Home extends React.Component {
     this.setState({ isOn: e });
   }
   render() {
-    //switch (this.state.currentPage) {
-    // case "Home":
-    return (
-      <div className="container" ref={(el) => (this.container = el)}>
-        <ApexCharts
-          className="currChart"
-          options={{
-            chart: {
-              id: "currentTemp",
-            },
-            plotOptions: {
-              radialBar: {
-                startAngle: -135,
-                endAngle: 135,
-                hollow: {
-                  size: "70%",
+    switch (this.state.currentPage) {
+      case "Home":
+        return (
+          <div className="container">
+            <ApexCharts
+              className="currChart"
+              options={{
+                chart: {
+                  id: "currentTemp",
                 },
-
-                dataLabels: {
-                  name: {
-                    show: false,
-                  },
-                  value: {
-                    formatter: function (val) {
-                      return parseFloat(val) + "°C";
+                plotOptions: {
+                  radialBar: {
+                    startAngle: -135,
+                    endAngle: 135,
+                    hollow: {
+                      size: "70%",
                     },
-                    color: "#fff",
-                    fontSize: "1em",
+
+                    dataLabels: {
+                      name: {
+                        show: false,
+                      },
+                      value: {
+                        formatter: function (val) {
+                          return parseFloat(val) + "°C";
+                        },
+                        color: "#fff",
+                        fontSize: "1em",
+                      },
+                    },
                   },
                 },
-              },
-            },
-            fill: {
-              type: "gradient",
-              gradient: {
-                shade: "dark",
-                type: "horizontal",
-                shadeIntensity: 0.5,
-                gradientToColors: ["#18cc96"],
-                inverseColors: true,
-                opacityFrom: 1,
-                opacityTo: 1,
-                stops: [0, 100],
-              },
-            },
-            stroke: {
-              lineCap: "round",
-            },
-            labels: ["Percent"],
-            grid: {
-              padding: {
-                bottom: 15,
-                top: 15,
-              },
-            },
-          }}
-          series={[this.state.currentTemperature]}
-          type="radialBar"
-          width={"100%"}
-        />
-        <div className="switch">
-          <Switch
-            checked={this.state.isOn}
-            onChange={this.toogleLight.bind(this)}
-            offColor="#ad1d25"
-            onColor="#86d3ff"
-            onHandleColor="#2693e6"
-            handleDiameter={70}
-            uncheckedIcon={false}
-            checkedIcon={false}
-            boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
-            activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
-            height={50}
-            width={100}
-          />
-        </div>
-        <div className="navbar">
-          <BottomNavigation
-            style={{ backgroundColor: "#111413" }}
-            value={this.state.currentPage}
-            onChange={this.navigationChange.bind(this)}
-          >
-            <BottomNavigationAction
-              style={{ color: "white" }}
-              label="Główna"
-              value="Home"
-              icon={<AiOutlineHome size={40} color={"white"} />}
+                fill: {
+                  type: "gradient",
+                  gradient: {
+                    shade: "dark",
+                    type: "horizontal",
+                    shadeIntensity: 0.5,
+                    gradientToColors: ["#18cc96"],
+                    inverseColors: true,
+                    opacityFrom: 1,
+                    opacityTo: 1,
+                    stops: [0, 100],
+                  },
+                },
+                stroke: {
+                  lineCap: "round",
+                },
+                labels: ["Percent"],
+                grid: {
+                  padding: {
+                    bottom: 15,
+                    top: 15,
+                  },
+                },
+              }}
+              series={[this.state.currentTemperature]}
+              type="radialBar"
+              width={"100%"}
             />
-            <BottomNavigationAction
-              style={{ color: "white" }}
-              label="Wykres"
-              value="Chart"
-              icon={<AiOutlineBarChart size={40} color={"white"} />}
-            />
-            <BottomNavigationAction
-              style={{ color: "white" }}
-              label="Ustawienia"
-              value="Settings"
-              icon={<AiOutlineSetting size={40} color={"white"} />}
-            />
-          </BottomNavigation>
-        </div>
-      </div>
-    );
-    //  break;
-    //}
+            <div className="switch">
+              <Switch
+                checked={this.state.isOn}
+                onChange={this.toogleLight.bind(this)}
+                offColor="#ad1d25"
+                onColor="#86d3ff"
+                onHandleColor="#2693e6"
+                handleDiameter={70}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                height={50}
+                width={100}
+              />
+            </div>
+            <div className="navbar">
+              <BottomNavigation
+                style={{ backgroundColor: "#111413" }}
+                value={this.state.currentPage}
+                onChange={this.navigationChange.bind(this)}
+              >
+                <BottomNavigationAction
+                  style={{ color: "white" }}
+                  label="Główna"
+                  value="Home"
+                  icon={<AiOutlineHome size={40} color={"white"} />}
+                />
+                <BottomNavigationAction
+                  style={{ color: "white" }}
+                  label="Wykres"
+                  value="Chart"
+                  icon={<AiOutlineBarChart size={40} color={"white"} />}
+                />
+                <BottomNavigationAction
+                  style={{ color: "white" }}
+                  label="Ustawienia"
+                  value="Settings"
+                  icon={<AiOutlineSetting size={40} color={"white"} />}
+                />
+              </BottomNavigation>
+            </div>
+          </div>
+        );
+        break;
+      case "Chart":
+        return (
+          <div className="container">
+            <HistoryChart data={this.state.historyTemperatures} />
+            <div className="nextChange">
+              <span className="nextChangeSpan">
+                {this.state.nextChangeText}
+              </span>
+            </div>
+            <div className="switch">
+              <Switch
+                checked={this.state.isOn}
+                onChange={this.toogleLight.bind(this)}
+                offColor="#ad1d25"
+                onColor="#86d3ff"
+                onHandleColor="#2693e6"
+                handleDiameter={70}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                height={50}
+                width={100}
+              />
+              <span className="lastChangeSpan">
+                {this.state.lastChangeText}
+              </span>
+            </div>
+            <div className="navbar">
+              <BottomNavigation
+                style={{ backgroundColor: "#111413" }}
+                value={this.state.currentPage}
+                onChange={this.navigationChange.bind(this)}
+              >
+                <BottomNavigationAction
+                  style={{ color: "white" }}
+                  label="Główna"
+                  value="Home"
+                  icon={<AiOutlineHome size={40} color={"white"} />}
+                />
+                <BottomNavigationAction
+                  style={{ color: "white" }}
+                  label="Wykres"
+                  value="Chart"
+                  icon={<AiOutlineBarChart size={40} color={"white"} />}
+                />
+                <BottomNavigationAction
+                  style={{ color: "white" }}
+                  label="Ustawienia"
+                  value="Settings"
+                  icon={<AiOutlineSetting size={40} color={"white"} />}
+                />
+              </BottomNavigation>
+            </div>
+          </div>
+        );
+        break;
+    }
   }
 }
 
